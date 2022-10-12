8e2010e1d0ec1eade8567adbd04c582d5c9c6db8
Nathaniel Pyon <nathaniel.pyon@mrjaffesclass.com>
2022-10-12 11:44:23 -0700

    work

---
+++ fizzbuzz.java
@@ -0,0 +1,13 @@
+public class fizzbuzz { //2 if else, 1 else 
+    public static void main (String[]args) {
+        double b;
+        double c;
+    
+        for (int a = 1; a <= 100 ; a++) {
+            b = a / 3;  c = a / 5;
+                if (b == (a) b && k == (a) c) {
+                    System.out.println(b)
+                }
+        }
+    }
+}
